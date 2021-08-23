680e0eb9e700e7d0dfac2c857e42b0b47beb2fd2
Bushev Vladimir <voldemarbush@gmail.com>
2021-08-23 23:25:42 +0300

    feat: make function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let result = '';
+    n = String(n);
+    for (let a = n.length-1; a >= 0; a--){
+        if (n[a] != '-') result += n[a];
+    }
+    return result;
 }
